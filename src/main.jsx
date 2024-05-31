efdf6d2fe0c07fce84f993d5b439c5782484d8fc
Barshon <barshonweb@gmail.com>
2024-05-31 17:40:06 +0600

    react router setup and route configure with layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './layout/Main';
+import Home from './Home/Home';
+import Login from './Authentication/Login';
+import Register from './Authentication/Register';
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      } 
+    ]
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/register',
+    element:<Register/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
